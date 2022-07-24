5b52f92091136e5e5c86122913a8f3e45b22dde5
franloz <francislozgut@gmail.com>
2022-07-24 17:12:33 +0200

    inicio fin

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
+import { ResponsiveService } from 'src/app/shared/services/responsive.service';
+
 
 @Component({
   selector: 'app-home',
@@ -8,12 +12,41 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  listaMovil:boolean=false;
+
+
+
+  profileForm: FormGroup;
+
+  constructor(private router: Router,private responsive:ResponsiveService) {
+    //formulario
+    this.profileForm = new FormGroup({//formulario reactivo
+      nombre: new FormControl('', Validators.required),
+      correo: new FormControl('',[Validators.required, Validators.email]),
+    });
+   }
 
   ngOnInit(): void {
+
+    this.responsive.cambioTamano().subscribe((result)=>{
+      this.listaMovil=false;
+      //this.listaOrdena=true;
+
+      if(result.matches){
+        this.listaMovil=true;//si llega al ancho pedido se esconde el menu para el movil
+       // this.listaOrdena=false;
+      }
+    })
+
+
   }
 
+  async onSubmit() {
+    ///buscarrrrrrr
+    this.profileForm.reset();
 
+
+  }
   reservar():void{
     this.router.navigate(['reservar']);
   }
